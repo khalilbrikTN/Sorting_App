d98e2ad84713599d95d33d24ef773f27b4b58d95
yahia <y_kilany@aucegypt.edu>
2023-05-14 16:39:49 +0300

    added The search functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QRandomGenerator>
+#include <QMessageBox>
+#include <chrono>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +13,80 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_GenerateDataset_clicked()
+{
+    QString num=ui->textEdit->toPlainText();
+    size=num.toInt();
+    ui->DatasetSize->setText("The Dataset size is: "+num);
+    QRandomGenerator generator = QRandomGenerator::securelySeeded();
+
+       // Generate a list of integers
+       for (int i = 0; i < size; ++i)
+       {
+           int number = generator.generate();
+           numbers.append(number);
+       }
+       ui->textEdit->clear();
+}
+
+
+void MainWindow::on_NormalSearch_clicked()
+{
+    useNormalSearch=true;
+    useBinarySearch=false;
+    ui->SearchAlgorithm->setText("using Normal Search");
+}
+
+
+void MainWindow::on_BinarySearch_clicked()
+{
+    useNormalSearch=false;
+    useBinarySearch=true;
+    ui->SearchAlgorithm->setText("using Binary Search");
+}
+
+bool MainWindow::BinarySearch(QList<int> list,int key,int start,int end)
+{
+    if(start>=end)
+    {
+        return false;
+    }
+    int mid= (start+end)/2;
+    if(list[mid]<key)
+    {
+        return BinarySearch(list,key,start,mid-1);
+    }
+    else if(list[mid]>key)
+    {
+        return BinarySearch(list,key,mid+1,end);
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void MainWindow::on_Findit_clicked()
+{
+    key=ui->textEdit_2->toPlainText().toInt();
+
+    if(useBinarySearch)
+    {
+        std::chrono::steady_clock::time_point now =std::chrono::steady_clock::now();
+        BinarySearch(this->numbers,key,0,this->numbers.size()-1);
+        std::chrono::steady_clock::time_point then=std::chrono::steady_clock::now();
+        this->sortTime=std::chrono::duration_cast<std::chrono::nanoseconds>(then-now).count();
+    }
+    if(useNormalSearch)
+    {
+
+    }
+    QMessageBox::information(
+           this,
+           tr("Application Name"),
+           tr("An information message.") );
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QList>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +15,24 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_GenerateDataset_clicked();
+
+    void on_NormalSearch_clicked();
+
+    void on_BinarySearch_clicked();
+
+    void on_Findit_clicked();
+
 private:
     Ui::MainWindow *ui;
+    int size;
+    int key;
+    int SearchTime;
+    int sortTime;
+    QList<int> numbers;
+    bool useBinarySearch=false;
+    bool useNormalSearch=false;
+    bool BinarySearch(QList<int>,int,int,int);
 };
 #endif // MAINWINDOW_H
